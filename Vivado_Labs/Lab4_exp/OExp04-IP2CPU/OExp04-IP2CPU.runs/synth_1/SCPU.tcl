fc768cc5a14fd4d5c06c493352f9442ca2c88a2f
dongqianyu99 <2015475633@qq.com>
2024-12-08 17:08:06 +0800

    Lab4.3

--- Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/SCPU.tcl
+++ Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/SCPU.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/SCPU.tcl"
+  variable script "D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/SCPU.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,8 +70,6 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_msg_config -id {Common 17-41} -limit 10000000
-set_msg_config -id {HDL-1065} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a100tcsg324-1
 
@@ -79,8 +77,8 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.cache/wt [current_project]
-set_property parent.project_path D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.xpr [current_project]
+set_property webtalk.parent_dir D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.cache/wt [current_project]
+set_property parent.project_path D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_repo_paths {
@@ -89,24 +87,24 @@ set_property ip_repo_paths {
   d:/Vivado_Labs/Lab4
 } [current_project]
 update_ip_catalog
-set_property ip_output_repo d:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.cache/ip [current_project]
+set_property ip_output_repo d:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_verilog -library xil_defaultlib {
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/ALU.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/ImmGen.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/REG32.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/add_32.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/my_DataPath.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/my_SCPU_ctrl.v
-  D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/SCPU.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/ALU.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/ImmGen.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/REG32.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/add_32.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/my_DataPath.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/my_SCPU_ctrl.v
+  D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/new/SCPU.v
 }
-read_ip -quiet D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/Regs_0/Regs_0.xci
+read_ip -quiet D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/Regs_0/Regs_0.xci
 
-read_ip -quiet D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/MUX2T1/MUX2T1.xci
+read_ip -quiet D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/MUX2T1/MUX2T1.xci
 
-read_ip -quiet D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/MUX4T1/MUX4T1.xci
+read_ip -quiet D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.srcs/sources_1/ip/MUX4T1/MUX4T1.xci
 
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
--- Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/runme.sh
+++ Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1/runme.sh
@@ -25,7 +25,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='D:/Vivado_Labs/Lab4/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1'
+HD_PWD='D:/Vivado_Labs/Lab4_exp/OExp04-IP2CPU/OExp04-IP2CPU.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
