179ff8ec923a7bb00851f4198b9a0f6c5ebbb2cf
dongqianyu99 <2015475633@qq.com>
2024-12-15 22:46:41 +0800

    Lab1

--- Vivado_Labs/Lab1/OExp01-Regs/OExp01-Regs.srcs/sim_1/new/Regs_tb.v
+++ Vivado_Labs/Lab1/OExp01-Regs/OExp01-Regs.srcs/sim_1/new/Regs_tb.v
@@ -30,6 +30,39 @@ module Regs_tb;
 				reg RegWrite; 
 				wire [31:0] Rs1_data; 
 				wire [31:0] Rs2_data;
+
+                wire [31: 0] Reg00;
+                wire [31: 0] Reg01;
+                wire [31: 0] Reg02;
+                wire [31: 0] Reg03;
+                wire [31: 0] Reg04;
+                wire [31: 0] Reg05;
+                wire [31: 0] Reg06;
+                wire [31: 0] Reg07;
+                wire [31: 0] Reg08;
+                wire [31: 0] Reg09;
+                wire [31: 0] Reg10;
+                wire [31: 0] Reg11;
+                wire [31: 0] Reg12;
+                wire [31: 0] Reg13;
+                wire [31: 0] Reg14;
+                wire [31: 0] Reg15;
+                wire [31: 0] Reg16;
+                wire [31: 0] Reg17;
+                wire [31: 0] Reg18;
+                wire [31: 0] Reg19;
+                wire [31: 0] Reg20;
+                wire [31: 0] Reg21;
+                wire [31: 0] Reg22;
+                wire [31: 0] Reg23;
+                wire [31: 0] Reg24;
+                wire [31: 0] Reg25;
+                wire [31: 0] Reg26;
+                wire [31: 0] Reg27;
+                wire [31: 0] Reg28;
+                wire [31: 0] Reg29;
+                wire [31: 0] Reg30;
+                wire [31: 0] Reg31;
 Regs Regs_U(
       .clk(clk),
       .rst(rst),
@@ -39,7 +72,39 @@ Regs Regs_U(
       .Wt_data(Wt_data),
       .RegWrite(RegWrite),
       .Rs1_data(Rs1_data),
-      .Rs2_data(Rs2_data)
+      .Rs2_data(Rs2_data),
+      .Reg00(Reg00),
+      .Reg01(Reg01),
+      .Reg02(Reg02),
+      .Reg03(Reg03),
+      .Reg04(Reg04),
+      .Reg05(Reg05),
+      .Reg06(Reg06),
+      .Reg07(Reg07),
+      .Reg08(Reg08),
+      .Reg09(Reg09),
+      .Reg10(Reg10),
+      .Reg11(Reg11),
+      .Reg12(Reg12),
+      .Reg13(Reg13),
+      .Reg14(Reg14),
+      .Reg15(Reg15),
+      .Reg16(Reg16),
+      .Reg17(Reg17),
+      .Reg18(Reg18),
+      .Reg19(Reg19),
+      .Reg20(Reg20),
+      .Reg21(Reg21),
+      .Reg22(Reg22),
+      .Reg23(Reg23),
+      .Reg24(Reg24),
+      .Reg25(Reg25),
+      .Reg26(Reg26),
+      .Reg27(Reg27),
+      .Reg28(Reg28),
+      .Reg29(Reg29),
+      .Reg30(Reg30),
+      .Reg31(Reg31)
 );
 
 always #10 clk = ~clk;
@@ -61,9 +126,18 @@ initial begin
      Wt_addr = 5'b01010;
      Wt_data = 32'h5a5a5a5a;
      #50
+     Wt_addr = 5'b10101;
+     Wt_data = 32'h9f9f9f9f;
+     #50
+     Wt_addr = 5'b11111;
+     Wt_data = 32'hf9f9f9f9;
+     #50
      RegWrite = 0;
      Rs1_addr = 5'b00101;
      Rs2_addr = 5'b01010;
+     #50
+     Rs1_addr = 5'b11111;
+     Rs2_addr = 5'b10101;
     
     #100 $stop();
 
